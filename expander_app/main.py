d2ab7c4e47c00aa45deda4776cb81bf47bd6f401
sauxpa <patrick.jr.saux@gmail.com>
2020-03-31 16:08:04 +0200

    add heart

--- expander_app/main.py
+++ expander_app/main.py
@@ -10,6 +10,17 @@ from bokeh.models.widgets import Slider, Tabs, Div
 from bokeh.models.graphs import from_networkx
 from bokeh.layouts import row, WidgetBox
 
+# LAYOUT_SHAPE = 'HEART'
+LAYOUT_SHAPE = ''
+
+def r(t):
+    """Polar coordinates of the graph layout.
+    """
+    if LAYOUT_SHAPE == 'HEART':
+        return (np.sin(t)*np.sqrt(np.abs(np.cos(t)))/(np.sin(t)+7/5)-2*np.sin(t)+2)
+    else:
+        return 1.0
+
 
 def make_dataset_lps3(p, div_):
     """Creates a ColumnDataSource object with data to plot.
@@ -56,8 +67,12 @@ def update_lps3(attr, old, new):
 
     # 1. First update layout
     circ = [i*2*np.pi/p for i in node_indices]
-    x = [np.cos(i) for i in circ]
-    y = [np.sin(i) for i in circ]
+    x = np.array([r(t) * np.cos(t) for t in circ])
+    y = np.array([r(t) * np.sin(t) for t in circ])
+    scale = np.max([x, y])
+    x = (x - np.mean(x)) / (scale - np.mean(x))
+    y = (y - np.mean(y)) / (scale - np.mean(y))
+
     graph_layout = dict(zip(node_indices, zip(x, y)))
     graph_renderer_lps3.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
@@ -86,8 +101,12 @@ def update_paley(attr, old, new):
 
     # 1. First update layout
     circ = [i*2*np.pi/p for i in node_indices]
-    x = [np.cos(i) for i in circ]
-    y = [np.sin(i) for i in circ]
+    x = np.array([r(t) * np.cos(t) for t in circ])
+    y = np.array([r(t) * np.sin(t) for t in circ])
+    scale = np.max([x, y])
+    x = (x - np.mean(x)) / (scale - np.mean(x))
+    y = (y - np.mean(y)) / (scale - np.mean(y))
+
     graph_layout = dict(zip(node_indices, zip(x, y)))
     graph_renderer_paley.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
@@ -134,8 +153,12 @@ graph_renderer_lps3 = from_networkx(G_lps3, nx.circular_layout, scale=1, center=
 
 node_indices_lps3 = list(range(p_lps3))
 circ = [i*2*np.pi/p_lps3 for i in node_indices_lps3]
-x = [np.cos(i) for i in circ]
-y = [np.sin(i) for i in circ]
+x = np.array([r(t) * np.cos(t) for t in circ])
+y = np.array([r(t) * np.sin(t) for t in circ])
+scale = np.max([x, y])
+x = (x - np.mean(x)) / (scale - np.mean(x))
+y = (y - np.mean(y)) / (scale - np.mean(y))
+
 graph_layout = dict(zip(node_indices_lps3, zip(x, y)))
 graph_renderer_lps3.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
@@ -188,8 +211,12 @@ graph_renderer_paley = from_networkx(G_paley, nx.circular_layout, scale=1, cente
 
 node_indices_paley = list(range(p_paley))
 circ = [i*2*np.pi/p_paley for i in node_indices_paley]
-x = [np.cos(i) for i in circ]
-y = [np.sin(i) for i in circ]
+x = np.array([r(t) * np.cos(t) for t in circ])
+y = np.array([r(t) * np.sin(t) for t in circ])
+scale = np.max([x, y])
+x = (x - np.mean(x)) / (scale - np.mean(x))
+y = (y - np.mean(y)) / (scale - np.mean(y))
+
 graph_layout = dict(zip(node_indices_paley, zip(x, y)))
 graph_renderer_paley.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
