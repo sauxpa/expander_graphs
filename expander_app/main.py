3970cc96381c8d735a5ced9b8c2bf1e22d2466b7
sauxpa <patrick.jr.saux@gmail.com>
2020-04-02 20:31:02 +0200

    add margulis

--- expander_app/main.py
+++ expander_app/main.py
@@ -65,6 +65,20 @@ def make_dataset_paley(p, div_):
     return ColumnDataSource(df)
 
 
+def make_dataset_margulis(m, div_):
+    """Creates a ColumnDataSource object with data to plot.
+    """
+    builder = Margulis(m)
+    builder.build()
+    df = nx.to_pandas_edgelist(builder.G)
+
+    params_text = '<b>Parameters:</b><br><ul><li>n = {}</li> <li>Number of nodes = {}</li> <li>Number of edges = {}</li></ul>'.format(m, builder.G.number_of_nodes(), builder.G.number_of_edges())
+    div_.text = params_text
+
+    # Convert dataframe to column data source#
+    return ColumnDataSource(df)
+
+
 def update_lps3(attr, old, new):
     """Update ColumnDataSource object.
     """
@@ -132,6 +146,38 @@ def update_paley(attr, old, new):
     graph_renderer_paley.edge_renderer.data_source.data = new_data_edge;
     graph_renderer_paley.node_renderer.data_source.data = new_data_nodes;
 
+
+def update_margulis(attr, old, new):
+    """Update ColumnDataSource object.
+    """
+    # Change p to selected value
+    m = m_select_margulis.value
+
+    # Create new graph
+    new_src = make_dataset_margulis(m, div_margulis)
+
+    # Update the data on the plot
+    src_margulis.data.update(new_src.data)
+
+    node_indices = list(range(m**2))
+
+    # 1. First update layout
+    circ = [i*2*np.pi/(m**2) for i in node_indices]
+    x = np.array([x_layout(t) for t in circ])
+    y = np.array([y_layout(t) for t in circ])
+    scale = np.max([x, y])
+    x = (x - np.mean(x)) / (scale - np.mean(x))
+    y = (y - np.mean(y)) / (scale - np.mean(y))
+
+    graph_layout = dict(zip(node_indices, zip(x, y)))
+    graph_renderer_margulis.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+    # 2. Then update nodes and edges
+    new_data_edge = {'start': src_margulis.data['source'], 'end': src_margulis.data['target']};
+    new_data_nodes = {'index': node_indices};
+    graph_renderer_margulis.edge_renderer.data_source.data = new_data_edge;
+    graph_renderer_margulis.node_renderer.data_source.data = new_data_nodes;
+
 ######################################################################
 ###
 ### Lubotsky-Phillips-Sarnak 3-regular
@@ -146,7 +192,7 @@ p_select_lps3 = Slider(start=2,
                        title='n-th prime number'
                        )
 
-# Update the plot when yields are changed
+# Update the plot when graph is changed
 p_select_lps3.on_change('value', update_lps3)
 
 pth = p_select_lps3.value
@@ -204,7 +250,7 @@ p_select_paley = Slider(start=1,
                         title='n-th Paley prime number'
                         )
 
-# Update the plot when yields are changed
+# Update the plot when graph is changed
 p_select_paley.on_change('value', update_paley)
 
 pth = p_select_paley.value
@@ -246,7 +292,62 @@ layout_paley = row(controls_paley, plot_paley)
 tab_paley = Panel(child=layout_paley, title='Paley')
 
 
+######################################################################
+###
+### Margulis 8-regular
+###
+######################################################################
+
+# Slider to select p
+m_select_margulis = Slider(start=2,
+                           end=25,
+                           step=1,
+                           value=5,
+                           title='Square root of the number of nodes'
+                       )
+
+# Update the plot when graph is changed
+m_select_margulis.on_change('value', update_margulis)
+
+m_margulis = m_select_margulis.value
+
+div_margulis = Div(text='<b>Parameters:</b><br>', width=200, height=100)
+
+src_margulis = make_dataset_margulis(m_margulis, div_margulis)
+
+plot_margulis = Plot(plot_width=600,
+                     plot_height=600,
+                     x_range=Range1d(-1.1,1.1),
+                     y_range=Range1d(-1.1,1.1)
+                     )
+
+plot_margulis.title.text = 'Margulis 8-regular graphs'
+G_margulis = nx.from_pandas_edgelist(pd.DataFrame(src_margulis.data))
+graph_renderer_margulis = from_networkx(G_margulis, nx.circular_layout, scale=1, center=(0,0))
+
+node_indices_margulis = list(range(m_margulis ** 2))
+circ = [i*2*np.pi/(m_margulis**2) for i in node_indices_margulis]
+x = np.array([x_layout(t) for t in circ])
+y = np.array([y_layout(t) for t in circ])
+scale = np.max([x, y])
+x = (x - np.mean(x)) / (scale - np.mean(x))
+y = (y - np.mean(y)) / (scale - np.mean(y))
+
+graph_layout = dict(zip(node_indices_margulis, zip(x, y)))
+graph_renderer_margulis.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+plot_margulis.renderers.append(graph_renderer_margulis)
+
+controls_margulis = WidgetBox(m_select_margulis, div_margulis)
+
+# Create a row layout
+layout_margulis = row(controls_margulis, plot_margulis)
+
+# Make a tab with the layout
+tab_margulis = Panel(child=layout_margulis, title='Margulis')
+
+
 ### ALL TABS TOGETHER
-tabs = Tabs(tabs=[tab_lps3, tab_paley])
+tabs = Tabs(tabs=[tab_lps3, tab_paley, tab_margulis])
 
 curdoc().add_root(tabs)
